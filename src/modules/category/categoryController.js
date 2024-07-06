8c21c87beb44293bfff88ce64e4528b8e1e47097
Nguyễn Quý Đôn <anctaone0511@gmail.com>
2024-07-06 10:52:41 +0700

    bringFromModelToService

--- src/modules/category/categoryController.js
+++ src/modules/category/categoryController.js
@@ -7,7 +7,7 @@ const addCategory = async (req, res, next) => {
     try {
         const profile = req.account
         const added = await categoryService.addCategory(req.body,profile)
-        SuccessRes(res, added, 'Add categor successful')
+        SuccessRes(res, added, 'Add category successful')
     } catch (error) {
         next(error)
     }
@@ -22,7 +22,7 @@ const getCates = async (req, res, next) => {
 }
 const deleteCate = async (req, res, next) => {
     try {
-        const deleted = await categoryModel.deleteCate(req.params.id)
+        const deleted = await categoryService.deleteCate(req.params.id)
         SuccessRes(res,deleted,'Deleted successful')
     } catch (error) {
         next(error)
--- src/modules/category/categoryService.js
+++ src/modules/category/categoryService.js
@@ -1,30 +1,61 @@
 import {Category, categoryModel} from "~/models/categoryModel"
 import slugify from "~/utils/stringToSlug"
+import ApiErr from "~/utils/ApiError";
+import {News} from "~/models/newsModel";
 
 const {accountModel} = require("~/models/accountModel")
 
 const addCategory = async (data, profile) => {
-    console.log(data)
-    console.log(profile)
-    //data = {name, createdBy}
-    // console.log(data)
-    const {name, type} = data
-    console.log('===============')
-    console.log(`name :${name}, type :${type})`)
-    console.log('===============')
-    const slug = slugify(name)
-    //
-    const cateExists = await Category.exists({name})
-    if (cateExists) {
-        throw new Error('Category name is exist')
+    try {
+        const {name, type} = data
+        const slug = slugify(name)
+        const cateExists = await Category.exists({name})
+        if (cateExists) {
+            throw new ApiErr(444, "Category already exists")
+        }
+        const category = new Category({name, slug, type})
+        category.createdBy = profile
+        await category.save()
+        return category
+    } catch (er) {
+        throw new Error(er)
     }
-    const category = new Category({name, slug, type})
-    category.createdBy = profile
-    console.log(category)
-    await category.save()
-    return category
+}
+const deleteCate = async (id) => {
+
+    const cate = await Category.findById(id)
+    if (!cate) {
+        throw new Error('Category not found')
+    }
+
+    const newsExists = await News.exists({categoryId: cate._id.toString()})
+    if (newsExists) {
+        throw new Error('Lỗi khóa ngoại')
+    }
+
+    await Category.findByIdAndDelete(id)
+
+    return true
+}
+const getCates = async () => {
+    const cates = await Category.find()
+    return cates
+}
+
+const updateCate = async (data) => {
+    const {id, name, updatedBy} = data
+
+    const updated = await Category.findByIdAndUpdate(id, {name, updatedBy})
+
+    if (!updated) {
+        throw new Error('Update fail')
+    }
+    return updated
 }
 
 export const categoryService = {
     addCategory,
+    deleteCate,
+    updateCate,
+    getCates,
 }
