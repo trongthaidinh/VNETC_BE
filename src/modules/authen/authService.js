d95fdd54e7fa660a24dfaf66080a21efdef75089
Nguyễn Quý Đôn <anctaone0511@gmail.com>
2024-07-16 10:40:28 +0700

    loginAccount

--- src/modules/authen/authService.js
+++ src/modules/authen/authService.js
@@ -1,21 +1,29 @@
-import { StatusCodes } from "http-status-codes"
-import { env } from "~/config/environment"
-import { jwtHelper } from "~/helper/jwtHelper"
-import { Account } from "~/models/accountModel"
+import {StatusCodes} from "http-status-codes"
+import {env} from "~/config/environment"
+import {jwtHelper} from "~/helper/jwtHelper"
+import {Account} from "~/models/accountModel"
 import ApiErr from "~/utils/ApiError"
 import bcrypt from 'bcryptjs'
 
 const login = async (email, password) => {
-    const account = await Account.findOne({email},{})
-    if (!account) {
-        throw new ApiErr(StatusCodes.NOT_FOUND,'Not found account')
-    }
-    if (bcrypt.compareSync(password,account.password)) {
-        throw new ApiErr(StatusCodes.CONFLICT,'Wrong password')
-    }
+    try {
+        const account = await Account.findOne({email}, {});
+        if (!account) {
+            throw new ApiErr(StatusCodes.NOT_FOUND, 'Account not found');
+        }
 
-    return await jwtHelper.generateToken(account, env.ACCESS_TOKEN_SECRET, env.ACCESS_TOKEN_LIFE)
-}
+        const passwordMatch = bcrypt.compareSync(password, account.password);
+        if (!passwordMatch) {
+            throw new ApiErr(StatusCodes.UNAUTHORIZED, 'Wrong password');
+        }
+
+        const token = await jwtHelper.generateToken(account, env.ACCESS_TOKEN_SECRET, env.ACCESS_TOKEN_LIFE);
+        return token;
+    } catch (error) {
+        throw error;
+    }
+};
+
 
 export const authService = {
     login
